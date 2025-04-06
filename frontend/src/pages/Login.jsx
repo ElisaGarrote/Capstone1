57eaeeef04bb5f61a5c125a0261470ea1a342d3a
Bengie Villesco <bvillesco@gmail.com>
2025-04-07 03:28:53 +0800

    Added codes. (Not yet final).

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,26 @@
-import Form from "../components/Form"
+//import Form from "../components/Form";
+import "../styles/Login.css";
+import loginImage from "../images/login_illustration.png";
 
 function Login() {
-    return <Form route="/api/token/" method="login" />
+  // return <Form route="/api/token/" method="login" />
+  return (
+    <main className="login-page">
+      <div className="left-panel">
+        <img src={loginImage} alt="login-illustration" />
+      </div>
+      <div className="right-panel">
+        <p>Username:</p>
+        <input type="text" placeholder="Enter your username" />
+
+        <p>Password:</p>
+        <input type="password" placeholder="Enter your password" />
+
+        <button type="submit">Log In</button>
+        <a href="#">Forgot Password?</a>
+      </div>
+    </main>
+  );
 }
 
-export default Login
+export default Login;
