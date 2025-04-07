efeadbd92c37539ebfed9cb657f931a83ced9502
Bengie Villesco <bvillesco@gmail.com>
2025-04-07 17:00:08 +0800

    Update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,30 +1,27 @@
 //import Form from "../components/Form";
 import "../styles/custom-colors.css";
 import "../styles/Login.css";
-import loginImage from "../images/login_illustration.png";
-import SubmitButton from "../components/button";
+import loginImage from "../images/login.png";
 
 function Login() {
   // return <Form route="/api/token/" method="login" />
   return (
     <main className="login-page">
-      <div className="left-panel">
+      <section className="left-panel">
         <img src={loginImage} alt="login-illustration" />
-      </div>
-      <div className="right-panel">
+      </section>
+      <section className="right-panel">
         <form action="" method="post">
           <p>Username:</p>
           <input type="text" placeholder="Enter your username" />
 
-          <p>Password:</p>
+          <p className="password">Password:</p>
           <input type="password" placeholder="Enter your password" />
 
           <button type="submit">Log In</button>
           <a href="#">Forgot Password?</a>
-
-          <SubmitButton />
         </form>
-      </div>
+      </section>
     </main>
   );
 }
