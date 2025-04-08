7d4bebe6538ab96c57d1179d210cd36086d9c52c
Edzra Macas <EdzraM7@gmail.com>
2025-04-08 17:17:31 +0800

    changed project structure and connected changed db from sqlite to postgresql

--- backend/backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("items/", views.ItemListCreate.as_view(), name="item-list"),
-    path("items/delete/<int:pk>/", views.ItemDelete.as_view(), name="delete-note"),
-]
