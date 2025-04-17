b3cc6009a1315d0e32291668b576eb59bd89f1a1
Bengie B. Villesco <bvillesco@gmail.com>
2025-04-17 17:18:40 +0800

    Added Table Buttons component with navigation

---
+++ frontend/src/components/buttons/TableButtons.jsx
@@ -0,0 +1,43 @@
+import "../../styles/custom-colors.css";
+import "../../styles/TableButtons.css";
+
+import { useNavigate } from "react-router-dom";
+import checkinIcon from "../../assets/icons/left.svg";
+import checkoutIcon from "../../assets/icons/right-arrow.svg";
+import editIcon from "../../assets/icons/edit.svg";
+import deleteIcon from "../../assets/icons/delete.svg";
+import viewIcon from "../../assets/icons/eye.svg";
+
+export default function TableButtons({ type, navigatePage }) {
+  let icon;
+  const navigate = useNavigate();
+
+  // Assign the correct icon based on the type
+  switch (type) {
+    case "checkout":
+      icon = checkoutIcon;
+      break;
+    case "checkin":
+      icon = checkinIcon;
+      break;
+    case "edit":
+      icon = editIcon;
+      break;
+    case "delete":
+      icon = deleteIcon;
+      break;
+    case "view":
+      icon = viewIcon;
+      break;
+  }
+
+  return (
+    <button
+      type="button"
+      className={`table-buttons-${type}`}
+      onClick={() => navigate(navigatePage)}
+    >
+      <img src={icon} alt="" />
+    </button>
+  );
+}
