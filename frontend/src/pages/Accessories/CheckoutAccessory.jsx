6cea12a769fffd6e2293aad48bed00176f701104
Bengie B. Villesco <bvillesco@gmail.com>
2025-04-17 17:21:16 +0800

    Added Checkout Accessory page for testing

---
+++ frontend/src/pages/Accessories/CheckoutAccessory.jsx
@@ -0,0 +1,15 @@
+import NavBar from "../../components/NavBar";
+import "../../styles/custom-colors.css";
+
+export default function CheckoutAccessory() {
+  return (
+    <>
+      <nav>
+        <NavBar />
+      </nav>
+      <main>
+        <h1>Checkout Accessory!</h1>
+      </main>
+    </>
+  );
+}
