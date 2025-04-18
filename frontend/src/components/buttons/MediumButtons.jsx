338a81e51d4f8504ba9944d90de3ea1fc006dd41
Bengie B. Villesco <bvillesco@gmail.com>
2025-04-19 00:50:35 +0800

    Added medium buttons component

---
+++ frontend/src/components/buttons/MediumButtons.jsx
@@ -0,0 +1,31 @@
+import "../../styles/custom-colors.css";
+import "../../styles/MediumButtons.css";
+
+import { useNavigate } from "react-router-dom";
+import plusIcon from "../../assets/icons/plus.svg";
+
+export default function MediumButtons({ type, navigatePage }) {
+  let icon;
+  const navigate = useNavigate();
+
+  // Assign the correct icon based on the type
+  switch (type.toLowerCase()) {
+    case "new":
+      icon = plusIcon;
+      break;
+    default:
+      icon = null;
+  }
+
+  return (
+    <button
+      type="button"
+      className={`medium-button-${type}`}
+      onClick={() => navigate(navigatePage)}
+    >
+      {/* The img tag will be rendered when the icon is not null*/}
+      {icon && <img src={icon} alt="" />}
+      {type}
+    </button>
+  );
+}
