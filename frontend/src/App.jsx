453479aa3c84f0ed1eb8b24ae198b9b5e12cb2c8
Bengie B. Villesco <bvillesco@gmail.com>
2025-04-20 22:02:15 +0800

    Added route for checkin accessory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
 import Accessories from "./pages/Accessories/Accessories";
+import CheckinAccessory from "./pages/accessories/CheckinAccessory";
 import CheckoutAccessory from "./pages/accessories/CheckoutAccessory";
 import AssetAudits from "./pages/asset-audit/AssetAudits";
 import OverdueAudits from "./pages/asset-audit/OverdueAudits";
@@ -41,12 +42,16 @@ function App() {
         <Route path="/register" element={<RegisterAndLogout />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/accessories" element={<Accessories />} />
+        <Route path="/accessories/checkin" element={<CheckinAccessory />} />
         <Route path="/accessories/checkout" element={<CheckoutAccessory />} />
         <Route path="/audits/" element={<AssetAudits />} />
         <Route path="/audits/overdue" element={<OverdueAudits />} />
         <Route path="/audits/scheduled" element={<ScheduledAudits />} />
         <Route path="/audits/completed" element={<CompletedAudits />} />
-        <Route path="/accessories/registration" element={<AccessoriesRegistration />} />
+        <Route
+          path="/accessories/registration"
+          element={<AccessoriesRegistration />}
+        />
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
