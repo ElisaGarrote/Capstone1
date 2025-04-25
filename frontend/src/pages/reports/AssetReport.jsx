872c311a36cb07b5be04a179f2a4cb5317372a45
JohannaSillano <sillanojohannamae@gmail.com>
2025-04-25 15:33:47 +0800

    Fixed the issue.

--- frontend/src/pages/reports/AssetReport.jsx
+++ frontend/src/pages/reports/AssetReport.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import NavBar from "../../components/NavBar";
-import FilterForm from "../../components/FilterForm";
 import "../../styles/reports/AssetReport.css";
 
 export default function AssetReport() {
