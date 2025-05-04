2129e2d8a1a3c512800adfc56db064c2ea24afe6
Edzra Macas <EdzraM7@gmail.com>
2025-05-05 04:19:44 +0800

    accessories db update

--- backend/accessories/accessories_ms/apps.py
+++ backend/accessories/accessories_ms/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AccessoriesServiceConfig(AppConfig):
+class AccessoriesMsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accessories_service'
+    name = 'accessories_ms'
