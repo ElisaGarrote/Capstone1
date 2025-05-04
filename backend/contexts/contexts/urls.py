66658e7a681c34833edece5a072cd9a6f0c12313
Edzra Macas <EdzraM7@gmail.com>
2025-05-05 05:45:31 +0800

    added contexts ms and url updates

--- backend/contexts/contexts/urls.py
+++ backend/contexts/contexts/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for accounts project.
+URL configuration for contexts project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('accounts_ms.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
