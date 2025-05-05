2699eaa8e03a945de542be75c4e3eca101d6dcfb
Edzra Macas <EdzraM7@gmail.com>
2025-05-05 08:52:27 +0800

    final accounts ms db structure

--- backend/accounts/accounts_ms/models.py
+++ backend/accounts/accounts_ms/models.py
@@ -7,22 +7,20 @@ class User(models.Model):
     ]
 
     first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50, blank=True)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, unique=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='operator')    
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)    
     contact_number = models.CharField(max_length=13)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.first_name} {self.middle_name} {self.last_name}".strip()
     
-class UserImages(models.Model):
+class UserImage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(null=True, blank=True, upload_to='user_images/')
+    image = models.ImageField(upload_to='user_images/')
     is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
