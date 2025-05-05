b619d867cd4b5a6314df6ac27f325d799aa2c0d0
Bengie B. Villesco <bvillesco@gmail.com>
2025-05-06 07:15:18 +0800

    Backend: Added urls path for related assets and audits

--- backend/assets/assets_ms/urls.py
+++ backend/assets/assets_ms/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # Products
     path('all-products/', get_products, name='get_products'),
-    path('create-product/', create_product, name='create_product')
+    path('create-product/', create_product, name='create_product'),
+
+    # Assets
+    path('create-asset/', create_asset, name='create_asset'),
+    path('all-asset/', get_all_assets, name='get_all_assets'),
+
+    # Audits
+    path('audits/create/', create_audit, name='create_audit'),
+    path('audits/add/files/', add_audit_file, name='add_audit_file'),
+    path('audits/all/', get_all_audit, name='get_all_audit'),
+    path('audits/create-schedule/', create_audit_schedule, name='create_audit_schedule'),
+    path('audits/all/schedules/', get_all_audit_schedules, name='get_all_audit_schedule'),
 ]
