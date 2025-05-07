e8900ae1e2968e5007431de80f0421261973f413
Bengie B. Villesco <bvillesco@gmail.com>
2025-05-08 04:10:24 +0800

    Backend: Dynamically display all the completed audits

--- frontend/src/pages/asset-audit/CompletedAudtis.jsx
+++ frontend/src/pages/asset-audit/CompletedAudtis.jsx
@@ -4,12 +4,13 @@ import NavBar from "../../components/NavBar";
 import MediumButtons from "../../components/buttons/MediumButtons";
 import TableBtn from "../../components/buttons/TableButtons";
 import Status from "../../components/Status";
-import { useLocation } from "react-router-dom";
+import { data, useLocation } from "react-router-dom";
 import TabNavBar from "../../components/TabNavBar";
 import DeleteModal from "../../components/Modals/DeleteModal";
 import Alert from "../../components/Alert";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ExportModal from "../../components/Modals/ExportModal";
+import assetsService from "../../services/assets-service";
 
 export default function CompletedAudits() {
   const location = useLocation();
@@ -18,6 +19,28 @@ export default function CompletedAudits() {
   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
   const [isDeleteSuccess, setDeleteSucess] = useState(false);
   const [isExportModalOpen, setExportModalOpen] = useState(false);
+  const [auditData, setAuditData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  // Retrieve all the audit data.
+  useEffect(() => {
+    const fetchAllAudit = async () => {
+      const dataResponse = await assetsService.fetchAllAudits();
+
+      if (dataResponse) {
+        setAuditData(dataResponse);
+      }
+    };
+
+    fetchAllAudit();
+  }, []);
+
+  // Set the isLoading state to true
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 300);
+  }, []);
 
   return (
     <>
@@ -63,72 +86,83 @@ export default function CompletedAudits() {
           <section>
             <TabNavBar />
           </section>
-          <section className="container">
-            <section className="top">
-              <h2>Completed Audits</h2>
-              <div>
-                <form action="" method="post">
-                  <input type="text" placeholder="Search..." />
-                </form>
-                <MediumButtons
-                  type="export"
-                  deleteModalOpen={() => setExportModalOpen(true)}
-                />
-              </div>
+          {isLoading ? (
+            "Loading..."
+          ) : auditData.length > 0 ? (
+            <section className="container">
+              <section className="top">
+                <h2>Completed Audits</h2>
+                <div>
+                  <form action="" method="post">
+                    <input type="text" placeholder="Search..." />
+                  </form>
+                  <MediumButtons
+                    type="export"
+                    deleteModalOpen={() => setExportModalOpen(true)}
+                  />
+                </div>
+              </section>
+              <section className="middle">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>
+                        <input type="checkbox" name="" id="" />
+                      </th>
+                      <th>AUDIT DATE</th>
+                      <th>ASSET</th>
+                      <th>STATUS</th>
+                      <th>LOCATION</th>
+                      <th>PERFORM BY</th>
+                      <th>DELETE</th>
+                      <th>VIEW</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {auditData.map((data, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>
+                            <input type="checkbox" name="" id="" />
+                          </td>
+                          <td>{data.audit_date}</td>
+                          <td>
+                            {data.audit_schedule_info.asset_info.displayed_id} -{" "}
+                            {data.audit_schedule_info.asset_info.name}
+                          </td>
+                          <td>
+                            <Status type="undeployable" name="Being Repaired" />
+                          </td>
+                          <td>{data.location}</td>
+                          <td>Pia Piatos-Lim</td>
+                          <td>
+                            <TableBtn
+                              type="delete"
+                              showModal={() => {
+                                setDeleteModalOpen(true);
+                                setSelectedRowId(assetId);
+                              }}
+                            />
+                          </td>
+                          <td>
+                            <TableBtn
+                              type="view"
+                              navigatePage="/audits/view"
+                              id={`${assetId} - ${assetName}`}
+                              previousPage={location.pathname}
+                            />
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </section>
+              <section></section>
             </section>
-            <section className="middle">
-              <table>
-                <thead>
-                  <tr>
-                    <th>
-                      <input type="checkbox" name="" id="" />
-                    </th>
-                    <th>AUDIT DATE</th>
-                    <th>ASSET</th>
-                    <th>STATUS</th>
-                    <th>LOCATION</th>
-                    <th>PERFORM BY</th>
-                    <th>DELETE</th>
-                    <th>VIEW</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <input type="checkbox" name="" id="" />
-                    </td>
-                    <td>December 31, 2025</td>
-                    <td>
-                      {assetId} - {assetName}
-                    </td>
-                    <td>
-                      <Status type="undeployable" name="Being Repaired" />
-                    </td>
-                    <td>General Santos City</td>
-                    <td>Pia Piatos-Lim</td>
-                    <td>
-                      <TableBtn
-                        type="delete"
-                        showModal={() => {
-                          setDeleteModalOpen(true);
-                          setSelectedRowId(assetId);
-                        }}
-                      />
-                    </td>
-                    <td>
-                      <TableBtn
-                        type="view"
-                        navigatePage="/audits/view"
-                        id={`${assetId} - ${assetName}`}
-                        previousPage={location.pathname}
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </section>
-            <section></section>
-          </section>
+          ) : (
+            "No records available."
+          )}
         </section>
       </main>
     </>
