e77ba015e75ef340d689902f66418ecd80849e5b
Bengie B. Villesco <bvillesco@gmail.com>
2025-05-12 20:37:38 +0800

    Backend: Added endpoint for get all audit files and get and edit schedule by id.

--- backend/assets/assets_ms/urls.py
+++ backend/assets/assets_ms/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path('audits/create/', create_audit, name='create_audit'),
     path('audits/add/files/', add_audit_file, name='add_audit_file'),
     path('audits/all/', get_all_audit, name='get_all_audit'),
+    path('audits/all/files', get_all_audit_files, name='get_all_audit_files'),
     path('audits/create-schedule/', create_audit_schedule, name='create_audit_schedule'),
+    path('audits/get/edit/schedule/<int:id>/', get_edit_audit_schedule_by_id, name='get_edit_audit_schedule_by_id'),
     path('audits/all/schedules/', get_all_audit_schedules, name='get_all_audit_schedule'),
 ]
