a9ca22ca8b8bc08a67f51e963e3c91f78ca6d2f4
Bengie B. Villesco <bvillesco@gmail.com>
2025-05-14 11:32:11 +0800

    Backend: Dynamically display the data from the backend. (Not yet finish)

--- frontend/src/pages/asset-audit/ViewAudits.jsx
+++ frontend/src/pages/asset-audit/ViewAudits.jsx
@@ -7,12 +7,13 @@ import Status from "../../components/Status";
 export default function ViewAudits() {
   const location = useLocation();
 
-  // Retrieve the "id" value passed from the navigation state.
-  // If the "id" is not exist, the default value for this is "undifiend".
-  const id = location.state?.id;
+  // Retrieve the "data" value passed from the navigation state.
+  // If the "data" is not exist, the default value for this is "undifiend".
+  const data = location.state?.data;
   const previousPage = location.state?.previousPage;
 
   console.log("root: ", previousPage);
+  console.log("data:", data);
 
   // Function to assign the appropriate value for the root props in TopSecFormPage component
   const assignRoot = () => {
@@ -38,45 +39,62 @@ export default function ViewAudits() {
             root={assignRoot()}
             currentPage="View Audits"
             rootNavigatePage={previousPage}
-            title={id}
+            title={`${data.asset_info.displayed_id} - ${data.asset_info.name}`}
           />
         </section>
         <section className="view-audits-content">
           <fieldset>
-            <label htmlFor="asset">Date Created</label>
+            <label htmlFor="date-created">Date Created</label>
             <p>April 10, 2025</p>
           </fieldset>
+
           <fieldset>
-            <label htmlFor="asset">Status</label>
+            <label htmlFor="status">Status</label>
             <p>
               <Status type="deployable" name="Ready to Deploy" />
             </p>
           </fieldset>
+
           <fieldset>
             <label htmlFor="asset">Asset</label>
-            <p>Macbook Pro 16"</p>
+            <p>{data.asset_info.name}</p>
           </fieldset>
+
           <fieldset>
             <label htmlFor="asset-id">Asset ID</label>
-            <p>100019</p>
+            <p>{data.asset_info.displayed_id}</p>
           </fieldset>
-          <fieldset>
-            <label htmlFor="location">Location</label>
-            <p>Makati</p>
-          </fieldset>
+
+          {data.audit_info.length == 1 ? (
+            <fieldset>
+              <label htmlFor="location">Location</label>
+              <p>{data.audit_info[0].location}</p>
+            </fieldset>
+          ) : (
+            ""
+          )}
+
           <fieldset>
             <label htmlFor="audit-date">
               {previousPage != "/audits/completed" ? "Due Date" : "Audit Date"}
             </label>
-            <p>April 25, 2025</p>
+            <p>
+              {previousPage != "/audits/completed"
+                ? data.date
+                : data.audit_info[0].audit_date}
+            </p>
           </fieldset>
-          <fieldset>
-            <label htmlFor="next-audit-date">Next Audit Date</label>
-            <p>May 25, 2025</p>
-          </fieldset>
+
+          {previousPage == "/audits/completed" && (
+            <fieldset>
+              <label htmlFor="next-audit-date">Next Audit Date</label>
+              <p>{data.date}</p>
+            </fieldset>
+          )}
+
           <fieldset>
             <label htmlFor="notes">Notes</label>
-            <p>-</p>
+            <p>{data.notes == "" ? "-" : data.notes}</p>
           </fieldset>
         </section>
       </main>
