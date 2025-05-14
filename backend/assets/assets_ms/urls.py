9ee03bd34401d10b39ec3d8f5117782e20371ad5
Edzra Macas <EdzraM7@gmail.com>
2025-05-14 20:30:08 +0800

    .

--- backend/assets/assets_ms/urls.py
+++ backend/assets/assets_ms/urls.py
@@ -3,8 +3,11 @@ from .views import *
 
 urlpatterns = [
     # Products
-    path('all-products/', get_products, name='get_products'),
-    path('create-product/', create_product, name='create_product'),
+    path('products/', get_products, name='all_products'),
+    path('products/registration/', create_product, name='add_new_product'),
+    path('products/<int:id>/', get_product_by_id, name='product_details_of_id'),
+    path('products/delete/<int:id>', soft_delete_product, name='delete_product'),
+    path('depreciations/product_registration', get_product_depreciations, name='depreciation_names'),
 
     # Assets
     path('create-asset/', create_asset, name='create_asset'),
--- backend/assets/assets_ms/views.py
+++ backend/assets/assets_ms/views.py
@@ -8,9 +8,39 @@ from .serializer import *
 # PRODUCTS HERE
 @api_view(['GET'])
 def get_products(request):
-    users = Product.objects.all()
-    serializedData = ProductSerializer(users, many=True).data
-    return Response(serializedData)
+    products = Product.objects.filter(is_deleted=False)
+    serializedProducts = AllProductSerializer(products, many=True).data
+    return Response(serializedProducts)
+
+@api_view(['GET', 'PUT'])
+def get_product_by_id(request, id):
+    try:
+        product = Product.objects.get(pk=id, is_deleted=False)
+    except Product.DoesNotExist:
+        return Response({'detail': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        # Check for image removal flag
+        remove_image = request.data.get('remove_image')
+        if remove_image == 'true' and product.image:
+            product.image.delete(save=False)
+            product.image = None
+
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_product_depreciations(request):
+    depreciations = Depreciation.objects.filter(is_deleted=False)
+    serializedDepreciations = ProductDepreciationSerializer(depreciations, many=True).data
+    return Response(serializedDepreciations)
 
 @api_view(['POST'])
 def create_product(request):
@@ -29,6 +59,16 @@ def add_product_image(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def soft_delete_product(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+        product.is_deleted = True
+        product.save()
+        return Response({'detail': 'Product soft-deleted'})
+    except Product.DoesNotExist:
+        return Response({'detail': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
 # END PRODUCT
 
 
