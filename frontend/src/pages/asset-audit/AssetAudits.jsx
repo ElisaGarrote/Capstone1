01c350a3adfdf7ea52a301085136ef3e11bc423d
Bengie B. Villesco <bvillesco@gmail.com>
2025-05-22 00:28:01 +0800

    Backend: - Update backend to dynamically display create at column

--- frontend/src/pages/asset-audit/AssetAudits.jsx
+++ frontend/src/pages/asset-audit/AssetAudits.jsx
@@ -12,7 +12,7 @@ import Alert from "../../components/Alert";
 import { useState, useEffect } from "react";
 import ExportModal from "../../components/Modals/ExportModal";
 import assetsService from "../../services/assets-service";
-import { formatDate } from "../../utils/dateFormatter";
+import dateRelated from "../../utils/dateRelated";
 
 export default function AssetAudits() {
   let notes = "sdfsdfsdfdfdfdfdfdfdfsdfsdfsdf";
@@ -186,7 +186,7 @@ export default function AssetAudits() {
                           <td>
                             <input type="checkbox" name="" id="" />
                           </td>
-                          <td>{formatDate(data.date)}</td>
+                          <td>{dateRelated.formatDate(data.date)}</td>
                           <td>
                             {data.asset_info.displayed_id} -{" "}
                             {data.asset_info.name}
@@ -194,7 +194,7 @@ export default function AssetAudits() {
                           <td>
                             <Status type="deployable" name="Ready to Deploy" />
                           </td>
-                          <td>December 30, 2025</td>
+                          <td>{dateRelated.formatDate(data.created_at)}</td>
                           <td>
                             <TableBtn
                               type="audit"
