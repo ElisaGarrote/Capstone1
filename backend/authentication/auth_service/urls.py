e00f30b4717e42c5228667cd0951bc9e78bcf08f
Bengie B. Villesco <bvillesco@gmail.com>
2025-05-24 21:22:54 +0800

    Backend: Add users url

--- backend/authentication/auth_service/urls.py
+++ backend/authentication/auth_service/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterViewset, api_test
+from .views import *
 
 router = DefaultRouter()
 router.register('register', RegisterViewset, basename='register')
+router.register('users', UsersViewset, basename='users')
 
 urlpatterns = [
     path('test/', api_test, name='api-test'),
