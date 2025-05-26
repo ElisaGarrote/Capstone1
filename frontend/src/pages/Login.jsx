5154b8b2aa4716a5053bc1d44d09881f52816e83
Edzra Macas <EdzraM7@gmail.com>
2025-05-26 20:30:24 +0800

    removed extra register button

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -164,17 +164,7 @@ function Login() {
               </button>
             </div>
           )}
-
-          <div className="form-btn">
-            <button
-              type="button"
-              onClick={() => navigate("/register")}
-              className="register-btn"
-            >
-              Register
-            </button>
-          </div>
-
+          
           <a onClick={() => navigate("/request/password_reset")}>
             Forgot Password?
           </a>
