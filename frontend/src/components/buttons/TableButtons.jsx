9f2085718725ff128763e1fe2db301bdd298854d
Bengie B. Villesco <bvillesco@gmail.com>
2025-05-29 03:34:04 +0800

    UI Update: - Add isDisabled props to represent the disable state of the button. - Conditionally render button (with and without disable state).

--- frontend/src/components/buttons/TableButtons.jsx
+++ frontend/src/components/buttons/TableButtons.jsx
@@ -1,7 +1,7 @@
 import "../../styles/custom-colors.css";
 import "../../styles/TableButtons.css";
 
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import checkinIcon from "../../assets/icons/left.svg";
 import checkoutIcon from "../../assets/icons/right-arrow.svg";
 import editIcon from "../../assets/icons/edit.svg";
@@ -16,28 +16,39 @@ export default function TableButtons({
   data,
   showModal,
   onClick,
+  isDisabled = false,
 }) {
   let icon;
+  let title;
   const navigate = useNavigate();
+  const location = useLocation();
 
   // Only log if data exists to avoid console spam
   // if (data !== undefined) {
   //   console.log("table button id received:", data);
   // }
 
-  // Assign the correct icon based on the type
+  const currentLocation = () => {
+    if (location.pathname.startsWith("/accessories")) {
+      return "accessory";
+    }
+  };
+
+  // Assign the correct icon and title based on the type
   switch (type) {
     case "checkout":
       icon = checkoutIcon;
       break;
     case "checkin":
       icon = checkinIcon;
+      title = `There are no items of this ${currentLocation()} checked out yet`;
       break;
     case "edit":
       icon = editIcon;
       break;
     case "delete":
       icon = deleteIcon;
+      title = "Cannot be deleted";
       break;
     case "view":
       icon = viewIcon;
@@ -47,26 +58,45 @@ export default function TableButtons({
       break;
   }
 
-  return (
-    <button
-      type="button"
-      className={`table-buttons-${type}`}
-      onClick={
-        onClick
-          ? onClick
-          : navigatePage != null
-          ? () => navigate(navigatePage, { state: { data, previousPage } })
-          : showModal
-      }
-      title={type === "audit" ? "Perform Audit" : null}
-    >
-      {type === "checkin" ? (
-        "Check-In"
-      ) : type === "checkout" ? (
-        "Check-Out"
-      ) : (
-        <img src={icon} alt="" />
-      )}
-    </button>
-  );
+  if (!isDisabled) {
+    return (
+      <button
+        type="button"
+        className={`table-buttons-${type}`}
+        onClick={
+          onClick
+            ? onClick
+            : navigatePage != null
+            ? () => navigate(navigatePage, { state: { data, previousPage } })
+            : showModal
+        }
+        title={type === "audit" ? "Perform Audit" : null}
+      >
+        {type === "checkin" ? (
+          "Check-In"
+        ) : type === "checkout" ? (
+          "Check-Out"
+        ) : (
+          <img src={icon} alt="icon" />
+        )}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        type="button"
+        className={`table-buttons-${type}-disabled`}
+        title={title}
+        disabled
+      >
+        {type === "checkin" ? (
+          "Check-In"
+        ) : type === "checkout" ? (
+          "Check-Out"
+        ) : (
+          <img src={icon} alt="icon" />
+        )}
+      </button>
+    );
+  }
 }
