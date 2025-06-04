acc0f2a85e52ace312ad6ad0bc7c00a5c3e4e51e
Edzra Macas <EdzraM7@gmail.com>
2025-06-04 22:36:00 +0800

    accessories 404 debug

--- backend/accessories/accessories/settings.py
+++ backend/accessories/accessories/settings.py
@@ -18,7 +18,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    'accessories-service-production.up.railway.app',
+    'accessories-service.railway.internal',
+    '*'
+]
 
 # Application definition
 INSTALLED_APPS = [
--- backend/accessories/accessories/wsgi.py
+++ backend/accessories/accessories/wsgi.py
@@ -1,14 +1,4 @@
-"""
-WSGI config for accessories project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
