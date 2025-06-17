3a0060a0f6221eeb8765cc43e5bf3c92643b5e0c
Edzra Macas <EdzraM7@gmail.com>
2025-06-18 03:58:10 +0800

    url update

--- backend/contexts/contexts_ms/urls.py
+++ backend/contexts/contexts_ms/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('suppliers/', get_suppliers, name='all_suppliers'),
     path('supplier/registration/', create_supplier, name='add_new_supplier'), 
 
-    path('manufacturers/', get_all_manufacturers, name='all_manufacturers'),
+    path('manufacturers/', get_all_manufacturers, name='get_all_manufacturers'),
     path('manufacturer/registration/', create_manufacturer, name='add_new_manufacturer'),
     path('manufacturers/<int:id>/update/', update_manufacturer, name='update_manufacturer_by_id'),
     path('manufacturers/<int:id>/delete/', soft_delete_manufacturer, name='soft_delete_manufacturer_by_id'),
