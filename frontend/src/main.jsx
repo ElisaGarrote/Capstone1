1931de07a80eceb5eca34effe04e3718d341d662
BengieVillesco <bvillesco@gmail.com>
2025-09-23 02:13:13 +0800

    Import fonrtawesome css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles/GlobalTableStyles.css'
-import './styles/GlobalStyles.css'
-import './styles/ModalFix.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/GlobalTableStyles.css";
+import "./styles/GlobalStyles.css";
+import "./styles/ModalFix.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
