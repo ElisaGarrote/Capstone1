9e730233823d03ab9e248751aaa77c0a9e77af03
BengieVillesco <bvillesco@gmail.com>
2025-09-26 03:06:22 +0800

    UI Enhancement: Revise UI

--- frontend/src/pages/reports/ActivityReport.jsx
+++ frontend/src/pages/reports/ActivityReport.jsx
@@ -1,156 +1,238 @@
-import React from 'react';
-import NavBar from '../../components/NavBar';
-import Status from '../../components/Status';
-import MediumButtons from '../../components/buttons/MediumButtons';
-import { BsKeyboard } from 'react-icons/bs';
-import { LuDroplet } from 'react-icons/lu';
-import { HiOutlineTag } from 'react-icons/hi';
-import { RxPerson } from 'react-icons/rx';
-import '../../styles/Reports.css';
-import '../../styles/UpcomingEndOfLife.css';
+import { useEffect, useRef, useState } from "react";
+import NavBar from "../../components/NavBar";
+import Status from "../../components/Status";
+import MediumButtons from "../../components/buttons/MediumButtons";
+import MockupData from "../../data/mockData/reports/activity-report-mockup-data.json";
+import DepreciationFilter from "../../components/FilterPanel";
+import Pagination from "../../components/Pagination";
+import { BsKeyboard } from "react-icons/bs";
+import { LuDroplet } from "react-icons/lu";
+import { HiOutlineTag } from "react-icons/hi";
+import { RxPerson } from "react-icons/rx";
+import { AiOutlineAudit } from "react-icons/ai";
+import { RxComponent1 } from "react-icons/rx";
 
-const ActivityReport = () => {
-  const activities = [
-    {
-      date: "Apr 2, 2023",
-      user: "Chippy McDonald",
-      type: "Accessory",
-      event: "Checkout",
-      item: "Magsafe Charger",
-      toFrom: "Xiaomie Ocho",
-      notes: "-"
-    },
-    {
-      date: "Apr 2, 2023",
-      user: "Renan Piotas",
-      type: "Consumable",
-      event: "Checkin",
-      item: "Magic Keyboard",
-      toFrom: "Xiaomie Ocho",
-      notes: "-"
-    },
-    {
-      date: "Apr 2, 2023",
-      user: "Xiaomie Ocho",
-      type: "Asset",
-      event: "Update",
-      item: "100003 - XPS13",
-      toFrom: "Xiaomie Ocho",
-      notes: "-"
-    },
-    {
-      date: "Apr 2, 2023",
-      user: "May Pomona",
-      type: "Asset",
-      event: "Checkout",
-      item: '100003 - MacBook Pro 16"',
-      toFrom: "Xiaomie Ocho",
-      notes: "-"
-    }
-  ];
+import "../../styles/reports/ActivityReport.css";
 
-  const getTypeIcon = (type) => {
-    switch (type) {
-      case 'Asset':
-        return <HiOutlineTag className="type-icon" />;
-      case 'Accessory':
-        return <BsKeyboard className="type-icon" />;
-      case 'Consumable':
-        return <LuDroplet className="type-icon" />;
-      default:
-        return <HiOutlineTag className="type-icon" />;
-    }
-  };
+const filterConfig = [
+  {
+    type: "select",
+    name: "type",
+    label: "Type",
+    options: [
+      { value: "accessory", label: "Accessory" },
+      { value: "asset", label: "Asset" },
+      { value: "audit", label: "Audit" },
+      { value: "component", label: "Component" },
+      { value: "consumable", label: "Consumable" },
+    ],
+  },
+  {
+    type: "select",
+    name: "event",
+    label: "Event",
+    options: [
+      { value: "checkin", label: "Checkin" },
+      { value: "checkout", label: "Checkout" },
+      { value: "create", label: "Create" },
+      { value: "delete", label: "Delete" },
+      { value: "failed", label: "Failed" },
+      { value: "passed", label: "Passed" },
+      { value: "repair", label: "Repair" },
+      { value: "schedule", label: "Schedule" },
+      { value: "update", label: "Update" },
+    ],
+  },
+  {
+    type: "text",
+    name: "user",
+    label: "User",
+  },
+  {
+    type: "text",
+    name: "tofrom",
+    label: "To/From",
+  },
+];
 
-  const getEventStatus = (event) => {
-    switch (event.toLowerCase()) {
-      case 'checkout':
-        return 'undeployable';
-      case 'checkin':
-        return 'deployable';
-      case 'update':
-        return 'pending';
-      default:
-        return 'pending';
+const getTypeIcon = (type) => {
+  switch (type) {
+    case "Asset":
+      return <HiOutlineTag className="type-icon" />;
+    case "Accessory":
+      return <BsKeyboard className="type-icon" />;
+    case "Consumable":
+      return <LuDroplet className="type-icon" />;
+    case "Audit":
+      return <AiOutlineAudit className="type-icon" />;
+    case "Component":
+      return <RxComponent1 className="type-icon" />;
+    default:
+      return <HiOutlineTag className="type-icon" />;
+  }
+};
+
+// TableHeader component to render the table header
+function TableHeader() {
+  return (
+    <tr>
+      <th>DATE</th>
+      <th>USER</th>
+      <th>TYPE</th>
+      <th>EVENT</th>
+      <th>ITEM</th>
+      <th>TO/FROM</th>
+      <th>NOTES</th>
+    </tr>
+  );
+}
+
+// TableItem component to render each ticket row
+function TableItem({ activity }) {
+  return (
+    <tr>
+      <td>{activity.date}</td>
+      <td>
+        <div className="user-name-activity-report">
+          <RxPerson className="user-icon" />
+          <span>{activity.user}</span>
+        </div>
+      </td>
+      <td>
+        <div className="type-info-activity-report">
+          {getTypeIcon(activity.type)}
+          <span>{activity.type}</span>
+        </div>
+      </td>
+      <td>
+        <Status type={activity.action.toLowerCase()} name={activity.action} />
+      </td>
+      <td>{activity.item}</td>
+      <td>
+        <div className="user-name-activity-report">
+          {activity.to_from ? <RxPerson className="user-icon" /> : "-"}
+          <span>{activity.to_from}</span>
+        </div>
+      </td>
+      <td>{activity.notes || "-"}</td>
+    </tr>
+  );
+}
+
+export default function ActivityReport() {
+  const [exportToggle, setExportToggle] = useState(false);
+  const exportRef = useRef(null);
+  const toggleRef = useRef(null);
+
+  // pagination state
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5); // default page size or number of items per page
+
+  // paginate the data
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedActivity = MockupData.slice(startIndex, endIndex);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        exportToggle &&
+        exportRef.current &&
+        !exportRef.current.contains(event.target) &&
+        toggleRef.current &&
+        !toggleRef.current.contains(event.target)
+      ) {
+        setExportToggle(false);
+      }
     }
-  };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [exportToggle]);
 
   return (
     <>
-      <NavBar />
-      <main className="reports-page">
-        <div className="reports-container">
-          <h1>Activity Report</h1>
+      <section>
+        <nav>
+          <NavBar />
+        </nav>
 
-          <div className="table-container">
-            <div className="table-header">
-              <div className="header-left">
-                <h2>Activity Log</h2>
-              </div>
-              <div className="header-right">
+        <main className="page-layout">
+          {/* Title of the Page */}
+          <section className="title-page-section">
+            <h1>Activity Report</h1>
+          </section>
+
+          {/* Table Filter */}
+          <DepreciationFilter filters={filterConfig} />
+
+          <section className="table-layout">
+            {/* Table Header */}
+            <section className="table-header">
+              <h2 className="h2">Activity Log ({MockupData.length})</h2>
+              <section className="table-actions">
                 <input
-                  type="text"
+                  type="search"
                   placeholder="Search..."
-                  className="search-input"
+                  className="search"
                 />
-                <MediumButtons type="export" navigatePage="" />
-              </div>
-            </div>
+                <div ref={toggleRef}>
+                  <MediumButtons
+                    type="export"
+                    onClick={() => setExportToggle(!exportToggle)}
+                  />
+                </div>
+              </section>
+            </section>
 
-            <table className="table activity-table activity-report-table">
-              <thead>
-                <tr>
-                  <th>DATE</th>
-                  <th>USER</th>
-                  <th>TYPE</th>
-                  <th>EVENT</th>
-                  <th>ITEM</th>
-                  <th>TO/FROM</th>
-                  <th>NOTES</th>
-                </tr>
-              </thead>
-              <tbody>
-                {activities.map((activity, index) => (
-                  <tr key={index}>
-                    <td><div className="cell-content">{activity.date}</div></td>
-                    <td><div className="cell-content">{activity.user}</div></td>
-                    <td>
-                      <div className="type-info">
-                        {getTypeIcon(activity.type)}
-                        <span>{activity.type}</span>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="status-container">
-                        <Status
-                          type={getEventStatus(activity.event)}
-                          name={activity.event}
-                        />
-                      </div>
-                    </td>
-                    <td>
-                      <div className="asset-cell">
-                        <a href="#" className="asset-link">
-                          {activity.item}
-                        </a>
-                      </div>
-                    </td>
-                    <td>
-                      <div className="user-info">
-                        <RxPerson className="user-icon" />
-                        <a href="#" className="user-link">{activity.toFrom}</a>
-                      </div>
-                    </td>
-                    <td><div className="cell-content">{activity.notes}</div></td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </main>
+            {/* Table Structure */}
+            <section className="activity-report-table-section">
+              {exportToggle && (
+                <section className="export-button-section" ref={exportRef}>
+                  <button>Download as Excel</button>
+                  <button>Download as PDF</button>
+                  <button>Download as CSV</button>
+                </section>
+              )}
+              <table>
+                <thead>
+                  <TableHeader />
+                </thead>
+                <tbody>
+                  {paginatedActivity.length > 0 ? (
+                    paginatedActivity.map((activity, index) => (
+                      <TableItem
+                        key={index}
+                        activity={activity}
+                        onDeleteClick={() => setDeleteModalOpen(true)}
+                      />
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan={7} className="no-data-message">
+                        No activity log found.
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </section>
+
+            {/* Table pagination */}
+            <section className="table-pagination">
+              <Pagination
+                currentPage={currentPage}
+                pageSize={pageSize}
+                totalItems={MockupData.length}
+                onPageChange={setCurrentPage}
+                onPageSizeChange={setPageSize}
+              />
+            </section>
+          </section>
+        </main>
+      </section>
     </>
   );
-};
-
-export default ActivityReport;
+}
