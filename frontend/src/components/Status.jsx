5a6296c1dd1dce54be25f58f6c77bd1309758563
BengieVillesco <bvillesco@gmail.com>
2025-10-10 18:45:43 +0800

    UI Update: Convert the div element into span element.

--- frontend/src/components/Status.jsx
+++ frontend/src/components/Status.jsx
@@ -27,22 +27,22 @@ export default function Status({
   */
 
   return (
-    <div className={`status-${type.split(" ").join("-")}`}>
-      <div className="circle"></div>
+    <span className={`status-${type.split(" ").join("-")}`}>
+      <span className="circle" aria-hidden="true"></span>
       {name}
       {/* Below will be rendered when any of these (i.e personName or location)
       is not equal to null*/}
       {(personName != null || location != null) && (
         <span className="status-details">
           <span className="status-to">to</span>
-          <div className="icon">
+          <span className="icon">
             <img src={personName != null ? personIcon : locationIcon} alt="" />
-          </div>
+          </span>
           <span className="status-target">
             {personName != null ? personName : location}
           </span>
         </span>
       )}
-    </div>
+    </span>
   );
 }
