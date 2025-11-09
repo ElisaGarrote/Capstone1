204b85e9a4809e148de99e6766af0487a50a9690
BengieVillesco <bvillesco@gmail.com>
2025-11-10 02:16:24 +0800

    Frontend: Use centralized auth url.

--- frontend/src/components/AxiosInstance.jsx
+++ frontend/src/components/AxiosInstance.jsx
@@ -1,17 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Authentication service
 //const baseUrl = 'https://authentication-service-production-d804.up.railway.app/'
 // Local development
-const baseUrl = "http://127.0.0.1:8001/";
+// const baseUrl = "http://127.0.0.1:8001/";
+const baseUrl = "http://127.0.0.1:8000/api/v1/";
 
 const axiosInstance = axios.create({
-    baseURL: baseUrl,
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-        accept: 'application/json'
-    },
-})
+  baseURL: baseUrl,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
+});
 
 export default axiosInstance;
