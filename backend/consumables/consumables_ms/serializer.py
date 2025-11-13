7e263317fdd4c7457486aa26548fed0a829e4fe8
Edzra Macas <EdzraM7@gmail.com>
2025-11-13 12:51:15 +0800

    .

--- backend/consumables/consumables_ms/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class ConsumableCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ConsumableCategory
-        fields = '__all__'
