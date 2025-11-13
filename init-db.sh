158c8a4b96f79d141657d2aa6d1dfe4f93a3d649
Edzra Macas <EdzraM7@gmail.com>
2025-11-13 12:54:02 +0800

    .

--- init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE ams_authentication;
-    CREATE DATABASE ams_assets;
-    CREATE DATABASE ams_contexts;
-EOSQL
