520b5941b11b5b6d4957d1a2f47302cf0f3060d6
Edzra Macas <EdzraM7@gmail.com>
2026-01-22 22:21:34 +0800

    tickets fetching path update

--- frontend/src/services/integration-ticket-tracking-service.js
+++ frontend/src/services/integration-ticket-tracking-service.js
@@ -5,18 +5,6 @@ import ticketTrackingAxios from "../api/integrationTicketTracking";
 ================================= */
 // GET all unresolved tickets (for checkout)
 export async function fetchAllTickets() {
-  const res = await ticketTrackingAxios.get("tickets/asset/unresolved/");
-  return res.data;
-}
-
-// GET all resolved tickets (for checkin history)
-export async function fetchResolvedTickets() {
-  const res = await ticketTrackingAxios.get("tickets/asset/resolved/");
-  return res.data;
-}
-
-// GET ticket by ID
-export async function fetchTicketById(id) {
-  const res = await ticketTrackingAxios.get(`tickets/asset/unresolved/`, { params: { id } });
+  const res = await ticketTrackingAxios.get("external/ams/tickets/");
   return res.data;
 }
