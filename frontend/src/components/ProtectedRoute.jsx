ecb38df8f0e1315795588867fbdc868c5e18466d
BengieVillesco <bvillesco@gmail.com>
2026-02-02 15:00:33 +0800

    frontend: Add console logs for debugging

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,7 +2,11 @@ import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { selectUser } from "../features/counter/userSlice";
 import { useSelector } from "react-redux";
-import { getUserFromToken, getUserRoleFromToken, getAccessToken } from "../api/TokenUtils";
+import {
+  getUserFromToken,
+  getUserRoleFromToken,
+  getAccessToken,
+} from "../api/TokenUtils";
 
 function ProtectedRoute({ roles }) {
   const user = useSelector(selectUser);
@@ -10,10 +14,11 @@ function ProtectedRoute({ roles }) {
   const userRole = getUserRoleFromToken();
   const currentUser = getUserFromToken();
   const token = getAccessToken();
-  // console.log("current user:", currentUser);
+  console.log("current user:", currentUser);
 
   // const role = currentUser?.role?.toLowerCase() || "";
   const role = currentUser?.roles?.[0]?.role?.toLowerCase() ?? "";
+  console.log("role:", role);
   const isAuthenticated = !!token;
 
   // Redirect the user back to the previous page.
